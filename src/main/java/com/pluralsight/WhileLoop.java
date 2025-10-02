646843958d966f4ddd253482b22bc4afca04372b
Dominick <dominickfrench.oe@gmail.com>
2025-10-02 12:45:40 -0500

    Second commit

--- src/main/java/com/pluralsight/WhileLoop.java
+++ src/main/java/com/pluralsight/WhileLoop.java
@@ -1,4 +1,12 @@
 package com.pluralsight;
 
 public class WhileLoop {
+    public static void main(String[] args) {
+        while() {
+
+
+        }
+
+        System.out.println("I love Java");
+    }
 }
