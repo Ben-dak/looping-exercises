ef8c42dada95f9090f621d678268e899e33e865c
Dominick <dominickfrench.oe@gmail.com>
2025-10-03 13:13:00 -0500

    DoWhile commit 2

--- src/main/java/com/pluralsight/DoWhileLoop.java
+++ src/main/java/com/pluralsight/DoWhileLoop.java
@@ -2,8 +2,11 @@ package com.pluralsight;
 
 public class DoWhileLoop {
     public static void main(String[] args) {
-        int count = 1;
+        int i = 0;
 
         do {
+            System.out.println("I love Java");
+            i++;
+        } while (i < 5);
     }
 }
